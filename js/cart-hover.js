d4329c4e50d2f6c3d61375e25f2f96c4be4e78e7
PawleN-D <sirpawle@gmail.com>
2020-04-17 12:42:48 +0200

    added the cart hover functionality across the website

--- js/cart-hover.js
+++ js/cart-hover.js
@@ -14,38 +14,39 @@ cartDisplay.addEventListener('mouseover', (e) => {
     }
 })
 
-function displayCart() {
-    let cartItems = localStorage.getItem('coursesInCart');
-    cartItems = JSON.parse(cartItems);
-    let courseContainer = document.querySelector('.cart-items');
-    // let cartCost = localStorage.getItem('totalCost');
-    if (cartItems && courseContainer) {
-        courseContainer.innerHTML = '';
-        Object.values(cartItems).map(course => {
-            courseContainer.innerHTML += `
-            <div class="row">
-            <div class="col-2 align-items-center pt-5">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="col-4 mb-4 pt-3">
-                <div class="course-thumb">
-                    <img class="img-responsive" src="./images/${course.name}.png">
-                </div>
-            </div>
-            <div class="col-6 p-2 align-items-center">
-                <div class="text-wrap pt-5">
-                    <p style="font-size: 13px;">${course.name}<br/>
-                        ${course.period}</p>
-                </div>
-            </div>
-        </div>   
 
-            `;
+// function displayCart() {
+//     let cartItems = localStorage.getItem('coursesInCart');
+//     cartItems = JSON.parse(cartItems);
+//     let courseContainer = document.querySelector('.cart-items');
+//     // let cartCost = localStorage.getItem('totalCost');
+//     if (cartItems && courseContainer) {
+//         courseContainer.innerHTML = '';
+//         Object.values(cartItems).map(course => {
+//             courseContainer.innerHTML += `
+//             <div class="row">
+//             <div class="col-2 align-items-center pt-5">
+//                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+//                     <span aria-hidden="true">&times;</span>
+//                 </button>
+//             </div>
+//             <div class="col-4 mb-4 pt-3">
+//                 <div class="course-thumb">
+//                     <img class="img-responsive" src="./images/${course.name}.png">
+//                 </div>
+//             </div>
+//             <div class="col-6 p-2 align-items-center">
+//                 <div class="text-wrap pt-5">
+//                     <p style="font-size: 13px;">${course.name}<br/>
+//                         ${course.period}</p>
+//                 </div>
+//             </div>
+//         </div>   
 
-        });
+//             `;
+
+//         });
     
         
-    }
-}
+//     }
+// }
